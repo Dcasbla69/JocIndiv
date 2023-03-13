75c01056ae05bf4881e4964e6cb68bb569764d98
Dcasbla69 <127753878+Dcasbla69@users.noreply.github.com>
2023-03-13 14:25:33 +0100

    Update Character.gd

--- Character.gd
+++ Character.gd
@@ -5,8 +5,8 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 var velocitat := 200
-var MaxJmp := 600
-var MinJmp := 200
+var MaxJmp := -600
+var MinJmp := -200
 var moviment := Vector2.ZERO
 var gravetat := Vector2.DOWN * 750
 var salt = Vector2.UP
@@ -27,24 +27,17 @@ func _process(delta):
 	if is_on_floor():
 		moviment.x = Input.get_axis("ves_esquerra","ves_dreta") * velocitat
 	
-	if Input.is_action_just_pressed("ves_amunt") and is_on_floor():
-		time += delta 
-		if time >= 2:
-			moviment.y = MaxJmp
+	if Input.is_action_pressed("ves_amunt") and is_on_floor():
+		time += delta
+		if time >= 0.1:
+			time = 0.1
+			if Input.is_action_just_released("ves_amunt"):
+				moviment.y += MaxJmp
+				time = 0
 		
 	if Input.is_action_just_released("ves_amunt") and is_on_floor():
-		moviment = ((MaxJmp - MinJmp)*(time/2) + MinJmp)
-#
-#	if CanJump and is_on_floor():
-#		if cumul_jmp > MaxJmp:
-#			cumul_jmp -= 15
-#
-#	if CanJump:
-#		velocity.x = cumul_dir
-#		velocity.y = cumul_jmp
-#		# reset
-#		cumul_jmp = 0
-#		cumul_dir = 0
+		moviment.y = ((MaxJmp - MinJmp)*(time/0.1) + MinJmp)
+		time = 0
 #
 	
 	moviment = move_and_slide(moviment, Vector2.UP)
